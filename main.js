19d547df3f12061e1a48cda7700855bc3ea7365f
saim514 <81713694+saim514@users.noreply.github.com>
2022-06-03 20:53:28 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,47 @@ drawn_sketch = "";
 answer_holder = "";
 score = 0;
 
+updateCanvas()
+{
+    canvas = background("white");
+}
+
+function setup()
+{
+    canvas = createCanvas(280,280);
+    canvas.center();
+    canvas = background("white");
+}
+
+function draw()
+{
+    check_sketch()
+    if( drawn_sketch == sketch)
+    {
+        answer_holder = "set";
+        score + 1;
+        document.getElementById("score").innerHTML = "Score: " + score; 
+    }
+}
+
+function check_sketch()
+{
+    timer_counter++;
+    document.getElementById("timer").innerHTML = "Timer: " + timer_counter;
+    console.log(timer_counter);
+    if(timer_counter == 1000)
+    {
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+
+    if(timer_check == "completed" || answer_holder == "set")
+    {
+        timer_check = "";
+        answer_holder = "";
+        update_canvas();
+    }
+
+}
+
+
